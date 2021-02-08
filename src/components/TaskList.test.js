0c8ec725c939965cc24baa233731fd5110863bdd
sallllly0307 <sara.love.choco.0307@gmail.com>
2021-02-09 01:30:40 +0900

    Add TaskList test

--- src/components/TaskList.test.js
+++ src/components/TaskList.test.js
@@ -3,3 +3,15 @@ import ReactDOM from 'react-dom';
 import '@testing-library/jest-dom/extend-expect';
 
 import { WithPinnedTasks } from './TaskList.stories';
+
+it('renders pinned tasks at the start of the list', () => {
+    const div = document.createElement('div');
+    //テストを使用させる
+    ReactDOM.render(<WithPinnedTasks {...WithPinnedTasks.args} />, div);
+  
+    //"task6（pinned）"というタイトルのタスクは、最後ではなく最初にレンダリングしたい
+    const lastTaskInput = div.querySelector('.list-item:nth-child(1) input[value="Task 6 (pinned)"]');
+    expect(lastTaskInput).not.toBe(null);
+  
+    ReactDOM.unmountComponentAtNode(div);
+  });
