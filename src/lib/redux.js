2b1b9460f13d3b0201b8e5448375e825a0a3a5e3
sallllly0307 <sara.love.choco.0307@gmail.com>
2021-05-05 23:59:58 +0900

    Update redux.js

--- src/lib/redux.js
+++ src/lib/redux.js
@@ -22,3 +22,14 @@ function taskStateReducer(taskState) {
       };
     };
   }
+  // The reducer describes how the contents of the store change for each action
+export const reducer = (state, action) => {
+    switch (action.type) {
+      case actions.ARCHIVE_TASK:
+        return taskStateReducer('TASK_ARCHIVED')(state, action);
+      case actions.PIN_TASK:
+        return taskStateReducer('TASK_PINNED')(state, action);
+      default:
+        return state;
+    }
+  };
